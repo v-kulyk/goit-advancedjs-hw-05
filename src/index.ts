068087c727ac7cfb1e161130d5ab813c1706c432
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-06-29 13:37:56 +0300

    add ts files structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-alert("Hello world!!!")
+import { concatenation } from "./concatenation";
+
+const button = document.querySelector("button")!;
+const input = document.querySelector("input")!;
+
+if (button && input) {
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello!");
+  });
+}
